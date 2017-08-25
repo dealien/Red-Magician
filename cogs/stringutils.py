467c7b9ff638b3f418c1bdcf0693aa42d6a24827
Red Magician <dragonofawsome@gmail.com>
2017-08-24 20:58:23 -0400

    auto-update via bash

--- cogs/stringutils.py
+++ cogs/stringutils.py
@@ -1,35 +1,14 @@
 import discord
 from discord.ext import commands
-from cogs.utils.dataIO import dataIO
 from collections import namedtuple, defaultdict, deque
-from datetime import datetime
 from copy import deepcopy
 from .utils import checks
 from cogs.utils.chat_formatting import pagify, box
 from enum import Enum
 from __main__ import send_cmd_help
 from discord.ext import commands
-import time
 import re
-import os
-from datetime import datetime, timedelta
 
-
-def paginate_string(content):
-    page = '```'
-    pages = []
-    for item in content:
-        if len(page + '\n' + item) > 1997:
-            page = page + '```'
-            pages.append(page)
-            page = '```'
-        page = page + '\n' + item
-    page = page + '```'
-    pages.append(page)
-    return pages
-
-def get_size(start_path = '.'):
-    return sum(os.path.getsize(os.path.join(dirpath,filename)) for dirpath, dirnames, filenames in os.walk(start_path) for filename in filenames)
 
 class StringFuncs:
     """A cog that provides various string analysis and manipulation commands."""
@@ -44,10 +23,12 @@ class StringFuncs:
             await send_cmd_help(ctx)
 
     @_file.command(pass_context=True)
-    @checks.serverowner_or_permissions(administrator=True)
-    async def list(self, ctx, server_id_or_substring=None):
-        """"""
+    async def length(self, ctx, input_string):
+        """Returns the length of the input string."""
+        await self.bot.say(str(len(input_string)))
         
 
 def setup(bot):
     bot.add_cog(StringFuncs(bot))
+
+pagify()
